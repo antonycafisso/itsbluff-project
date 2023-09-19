45b1935a7ad763ea76d3d0032795a1965b143a95
Tony Cafisso <tonycafisso@gmail.com>
2023-09-19 13:39:55 -0300

    Create project

---
+++ src/main/java/com/itsbluff/itsbluffproject/ItsbluffProjectApplication.java
@@ -0,0 +1,13 @@
+package com.itsbluff.itsbluffproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsbluffProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsbluffProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsbluff/itsbluffproject/ItsbluffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsbluff.itsbluffproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsbluffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
